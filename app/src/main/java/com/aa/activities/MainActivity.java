850b3ce182f5ecf8554eecabec76efb74974b92f
Anubhav Anand <aa7784@gmail.com>
2018-12-17 16:39:29 +0530

    on click in layout xml

--- app/src/main/java/com/aa/activities/MainActivity.java
+++ app/src/main/java/com/aa/activities/MainActivity.java
@@ -1,43 +1,28 @@
 package com.aa.activities;
 
 import android.graphics.Color;
-import android.support.constraint.ConstraintLayout;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity{
 
-    private static final String TAG = MainActivity.class.getSimpleName();
+    RelativeLayout relativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final RelativeLayout relativeLayout = findViewById(R.id.relativeLayout);
+        relativeLayout = findViewById(R.id.relativeLayout);
+    }
 
-        Button button1 = findViewById(R.id.button1);
-        Button button2 = findViewById(R.id.button2);
+    public void changeToGreen(View view) {
+        relativeLayout.setBackgroundColor(Color.GREEN);
+    }
 
-        assert button1 != null;
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                relativeLayout.setBackgroundColor(Color.GREEN);
-            }
-        });
-
-        assert button2 != null;
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                relativeLayout.setBackgroundColor(Color.BLUE);
-            }
-        });
-
+    public void changeToBlue(View view) {
+        relativeLayout.setBackgroundColor(Color.BLUE);
     }
 }

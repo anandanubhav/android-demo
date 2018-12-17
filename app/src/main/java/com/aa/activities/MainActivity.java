8857fc3fb2945c16cb83538186a6debe5cad55d3
Anubhav Anand <aa7784@gmail.com>
2018-12-17 15:13:06 +0530

    button on click listener

---
+++ app/src/main/java/com/aa/activities/MainActivity.java
@@ -0,0 +1,44 @@
+package com.aa.activities;
+
+import android.graphics.Color;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private RelativeLayout relativeLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        relativeLayout = findViewById(R.id.relativeLayout);
+
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+
+        button1.setOnClickListener(MainActivity.this);
+        button2.setOnClickListener(MainActivity.this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button1:
+                Log.i(TAG,"Color changed to GREEN");
+                relativeLayout.setBackgroundColor(Color.GREEN);
+                break;
+            case R.id.button2:
+                Log.i(TAG,"Color changed to BLUE");
+                relativeLayout.setBackgroundColor(Color.BLUE);
+                break;
+        }
+    }
+}
